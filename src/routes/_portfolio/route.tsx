33cd80ff8a3a497366b37ea43af1d4eab55abfa3
fminsaf <itsmefminsaf@gmail.com>
2026-09-04 22:57:05 +0530

    feat: enhance portfolio route with improved layout

--- src/routes/_portfolio/route.tsx
+++ src/routes/_portfolio/route.tsx
@@ -5,7 +5,8 @@ import {
 	useRouteContext,
 	useRouter,
 } from "@tanstack/react-router";
-import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { useEffect, useState } from "react";
+import { BsArrowLeft, BsGithub, BsLinkedin } from "react-icons/bs";
 import { CgInfo } from "react-icons/cg";
 import { FaMoon, FaSun } from "react-icons/fa";
 import Insaf from "#/assets/Insaf.jpg";
@@ -16,6 +17,8 @@ export const Route = createFileRoute("/_portfolio")({
 });
 
 function RouteComponent() {
+	const [cardOpen, setCardOpen] = useState(false);
+
 	const { theme } = useRouteContext({ from: "__root__" });
 	const router = useRouter();
 
@@ -24,31 +27,65 @@ function RouteComponent() {
 		return await router.invalidate();
 	};
 
+	useEffect(() => {
+		setTimeout(() => setCardOpen(true), 1000);
+		setTimeout(() => setCardOpen(true), 3000);
+	}, []);
+
 	return (
 		<div className="max-w-7xl mx-auto p-3">
-			<div className="w-80 fixed top-1/2 -translate-y-1/2">
-				<div className="space-y-5 p-3 rounded-xl bg-linear-150 from-stone-200 dark:from-stone-900 shadow-lg border-t border-zinc-100 dark:border-zinc-800">
-					<header className="space-y-3">
-						<div className="size-40 rounded-full overflow-hidden mx-auto">
-							<img src={Insaf} alt="Muhammad Insaf" />
+			<div
+				className={`${!cardOpen && "max-lg:w-16"} w-72 duration-300 fixed top-1/2 -translate-y-1/2 overflow-hidden`}
+			>
+				<div className="rounded-xl bg-linear-150 from-stone-200 dark:from-stone-900 backdrop-blur-2xl shadow-lg border-t border-zinc-100 dark:border-zinc-800 overflow-hidden">
+					<header className="space-y-3 p-2 lg:pt-5">
+						<div
+							className={`lg:hidden w-fit ${cardOpen ? "ml-auto" : "mx-auto"}`}
+						>
+							<button
+								type="button"
+								className="p-3 rounded-full bg-stone-100 dark:bg-stone-800 cursor-pointer"
+								onClick={() => setCardOpen(!cardOpen)}
+							>
+								<BsArrowLeft
+									size={25}
+									className={`${!cardOpen && "-scale-100"}`}
+								/>
+							</button>
 						</div>
-						<h1 className="text-xl font-extrabold text-center leading-tight">
-							Muhammad Insaf <br />
-							<span className="font-bold text-taupe-600 dark:text-taupe-500">
-								ibn Farsan
-							</span>
-						</h1>
-						<p className="text-sm text-center text-taupe-600 dark:text-taupe-400">
-							Your technical partner for fast, high-quality web applications
-							development
-						</p>
 
-						<div className="flex items-center justify-center gap-3">
+						<div className="mx-auto w-fit">
+							<div
+								className={`size-40 rounded-full overflow-hidden mx-auto ${!cardOpen && "max-lg:hidden"}`}
+							>
+								<img src={Insaf} alt="Muhammad Insaf" />
+							</div>
+
+							<h1
+								className={`text-xl font-extrabold text-center leading-tight ${!cardOpen && "max-lg:text-sm max-lg:[writing-mode:vertical-rl] max-lg:rotate-180"}`}
+							>
+								Muhammad Insaf <br />
+								<span className="font-semibold text-taupe-600 dark:text-taupe-500">
+									ibn Farsan
+								</span>
+							</h1>
+
+							<p
+								className={`w-56 text-sm text-center text-taupe-600 dark:text-taupe-400 ${!cardOpen && "max-lg:hidden"}`}
+							>
+								Your technical partner for fast, high-quality web applications
+								development
+							</p>
+						</div>
+
+						<div
+							className={`flex items-center justify-center gap-3 ${!cardOpen && "max-lg:flex-col"}`}
+						>
 							<a
 								href="http://linkedin.com/in/itsmefminsaf"
 								target="_blank"
 								rel="noopener noreferrer"
-								className="hover:-translate-y-1 hover:rotate-12 duration-300"
+								className={`${cardOpen && "hover:-translate-y-1 hover:rotate-12 duration-300"}`}
 							>
 								<BsLinkedin size={25} />
 							</a>
@@ -56,7 +93,7 @@ function RouteComponent() {
 								href="http://github.com/fminsaf"
 								target="_blank"
 								rel="noopener noreferrer"
-								className="hover:-translate-y-1 hover:rotate-12 duration-300"
+								className={`${cardOpen && "hover:-translate-y-1 hover:rotate-12 duration-300"}`}
 							>
 								<BsGithub size={25} />
 							</a>
@@ -66,24 +103,29 @@ function RouteComponent() {
 
 						<nav className="w-fit mx-auto">
 							<ul className="flex flex-col gap-2">
-								<li className="w-60">
+								<li>
 									<Link
 										to="/"
-										className="px-3 py-1 rounded-xl flex items-center gap-2"
+										className={`rounded-full flex items-center gap-2 px-3 py-1 ${!cardOpen && "max-lg:p-2"}`}
 										activeProps={{
 											className: "bg-stone-200 dark:bg-stone-800",
 										}}
+										onClick={() => setCardOpen(false)}
 									>
 										<CgInfo size={20} />
-										Who am I ?
+										<span className={`w-48 ${!cardOpen && "max-lg:hidden"}`}>
+											Who am I ?
+										</span>
 									</Link>
 								</li>
 							</ul>
 						</nav>
 					</header>
 
-					<footer className="flex items-center justify-center gap-3 text-xs font-semibold text-taupe-600 dark:text-taupe-400">
-						<span>© Muhammad Insaf</span>
+					<footer className="flex items-center justify-center gap-3 py-3 text-xs font-semibold bg-stone-200 dark:bg-stone-800 text-taupe-600 dark:text-taupe-400">
+						<span className={`${!cardOpen && "max-lg:hidden"}`}>
+							© Muhammad Insaf
+						</span>
 						<button
 							type="button"
 							onClick={switchTheme}
@@ -95,7 +137,7 @@ function RouteComponent() {
 				</div>
 			</div>
 
-			<main className="ml-80 p-3">
+			<main className="ml-16 lg:ml-72 p-3">
 				<Outlet />
 			</main>
 		</div>
