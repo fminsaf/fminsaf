85a0279a1d6b596dcf9048229df35856e10ced7f
fminsaf <itsmefminsaf@gmail.com>
2026-09-05 10:43:40 +0530

    feat: add hire-me and projects routes and update nav links

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,9 +1,13 @@
 import { Link, linkOptions } from "@tanstack/react-router";
 import type { Dispatch } from "react";
 import { CgInfo } from "react-icons/cg";
+import { FaHandshake } from "react-icons/fa";
+import { GoProject } from "react-icons/go";
 
 const navLinkList = linkOptions([
 	{ to: "/", text: "Who am I ?", Icon: CgInfo },
+	{ to: "/projects", text: "My Projects", Icon: GoProject },
+	{ to: "/hire-me", text: "Hire me", Icon: FaHandshake },
 ]);
 
 const NavLinks = ({
@@ -18,10 +22,13 @@ const NavLinks = ({
 			{navLinkList.map(({ to, text, Icon }) => (
 				<li key={to}>
 					<Link
-						to="/"
-						className={`rounded-full flex items-center gap-2 px-3 py-1 ${!cardOpen && "max-lg:p-2"}`}
+						to={to}
+						className={`rounded-full transition-colors duration-300 flex items-center gap-2 px-3 py-1 ${!cardOpen && "max-lg:p-2"}`}
 						activeProps={{
 							className: "bg-stone-200 dark:bg-stone-800",
+						}}
+						inactiveProps={{
+							className: "hover:bg-stone-200/50 hover:dark:bg-stone-800/50",
 						}}
 						onClick={() => setCardOpen(false)}
 					>
